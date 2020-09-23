94ea30cde93bd28f4e654d2b25496f25451e4950
Andrii Moisol <andrii.moisol@gmail.com>
2020-09-23 13:51:42 +0300

    Add middleware

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", myMiddleware(myHandler))
+
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("hello from handler")
+	w.WriteHeader(http.StatusOK)
+}
+
+func myMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("hello from middleware")
+		next(w, r)
+	}
 }
